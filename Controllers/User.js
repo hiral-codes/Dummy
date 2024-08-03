744084539eb9062d170ef8a15865af0d8fceabc3
Hiral Patel <hiralpatel160302@gmail.com>
2024-08-03 15:14:11 +0530

    added update route

--- Controllers/User.js
+++ Controllers/User.js
@@ -1,14 +1,15 @@
 import User from "../Models/user.model.js";
-import bcrypt from "bcrypt"
-import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt";
+import { use } from "bcrypt/promises.js";
+import jwt from "jsonwebtoken";
 
 export const createUser = async (req, res) => {
   try {
     const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10)  //For hashing The Password
+    const hashedPassword = await bcrypt.hash(password, 10); //For hashing The Password
     const user = new User({
       username,
-      password: hashedPassword
+      password: hashedPassword,
     });
     const SavedUser = await user.save();
     res.status(200).json(SavedUser);
@@ -52,27 +53,62 @@ export const getUser = async (req, res) => {
   }
 };
 
-
 // Login
 export const login = async (req, res) => {
   const { username, password } = req.body;
   try {
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(400).json({ error: 'Invalid credentials' });
+      return res.status(400).json({ error: "Invalid credentials" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password); //hashed password ko compare krne ke liye
     if (!isMatch) {
-      return res.status(400).json({ error: 'Invalid credentials' });
+      return res.status(400).json({ error: "Invalid credentials" });
     }
 
     const payload = { userId: user._id };
-    const token = jwt.sign(payload, "security_key_daal_dena", { expiresIn: '7d' }); //Yha Pe Security Key Daal Dena kuch bhi
+    const token = jwt.sign(payload, "security_key_daal_dena", {
+      expiresIn: "7d",
+    }); //Yha Pe Security Key Daal Dena kuch bhi
 
-    res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'strict', maxAge: 7 * 24 * 60 * 60 * 1000 }); // 7 days
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    }); // 7 days
     res.json(user); //response me user ka data bhi jayega usko access krke context me daal dene ka
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
+
+
+// Updating Profile
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { username } = req.body;
+
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    if (username) {
+      user.username = username;
+    }
+    // Other Fields as needed...
+    // if (age) {
+    //   user.age = age;
+    // }
+
+    const savedUser = await user.save();
+    res.status(200).json({ user: savedUser, message: "User Updated successfully" });
+  } catch (error) {
+    res.status(400).json({ message: "Error updating user", error: error.message });
+  }
+};
+
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import {createUser, login} from '../Controllers/User.js'
+import { createUser, login, updateUser } from '../Controllers/User.js'
 const router = express.Router();
 
 
-router.post("/register",createUser);
-router.post("/login",login)
+router.post("/register", createUser);
+router.post("/login", login)
+
+router.patch("/update/:id", updateUser)
 
 export default router;
 
